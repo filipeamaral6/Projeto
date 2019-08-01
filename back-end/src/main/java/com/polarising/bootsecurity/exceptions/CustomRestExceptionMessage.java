0ba79b8ee8fc376c1d9ee862a64dd63d11015d79
Filipe Amaral <filipe.amaral@polarising.com>
2019-08-01 16:20:52 +0100

    Exception Handling

---
+++ back-end/src/main/java/com/polarising/bootsecurity/exceptions/CustomRestExceptionMessage.java
@@ -0,0 +1,14 @@
+package com.polarising.bootsecurity.exceptions;
+
+public class CustomRestExceptionMessage extends RuntimeException{
+	
+	private static final long serialVersionUID = 5377334777759551943L;
+
+	public CustomRestExceptionMessage(String message) {
+		super(message);
+	}
+
+	public CustomRestExceptionMessage(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
