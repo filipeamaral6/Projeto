4ae22cc6aabce93ddc2b568cfcd39e5a0a8eaeab
Filipe Amaral <filipe.amaral@polarising.com>
2019-08-02 16:46:25 +0100

    Back-end with REST authentication

--- back-end/src/main/java/com/polarising/bootsecurity/controller/UserController.java
+++ back-end/src/main/java/com/polarising/bootsecurity/controller/UserController.java
@@ -1,26 +1,34 @@
 package com.polarising.bootsecurity.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.polarising.bootsecurity.db.UserRepository;
 import com.polarising.bootsecurity.model.User;
+import com.polarising.bootsecurity.security.SecurityConfiguration;
 
 @RestController
 @RequestMapping("/")
 @CrossOrigin
 public class UserController {
 
-	private UserRepository userRepository;
+//	private UserRepository userRepository;
+//
+//	public UserController(UserRepository userRepository) {
+//		this.userRepository = userRepository;
+//	}
 
-	public UserController(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
-
+	@Autowired
+	SecurityConfiguration securityConfiguration;
+	
     @GetMapping("admin")
     public String test1(){
         return "Admins only";
@@ -31,8 +39,15 @@ public class UserController {
         return "Users only";
     }
     
-    @GetMapping("users")
-    public List<User> users() {
-    	return this.userRepository.findAll();
+//    @GetMapping("users")
+//    public List<User> users() {
+//    	return this.userRepository.findAll();
+//    }
+    
+    @GetMapping("users-testing")
+    public List<User> usersTesting() {
+    	User user = new User("user", securityConfiguration.passwordEncoder().encode("user"), "ADMIN" );
+    	List<User> users = Arrays.asList(user);
+    	return users;
     }
 }
--- back-end/src/main/java/com/polarising/bootsecurity/security/UserPrincipalDetailsService.java
+++ back-end/src/main/java/com/polarising/bootsecurity/security/UserPrincipalDetailsService.java
@@ -1,28 +1,67 @@
 package com.polarising.bootsecurity.security;
 
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import com.polarising.bootsecurity.db.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.polarising.bootsecurity.exceptions.CustomRestExceptionMessage;
 import com.polarising.bootsecurity.model.User;
 
 @Service
 public class UserPrincipalDetailsService implements UserDetailsService {
-	private UserRepository userRepository;
-	
-	public UserPrincipalDetailsService(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
-	
-	@Override
-	public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-		User user = this.userRepository.findByUsername(s);
-		UserPrincipal userPrincipal = new UserPrincipal(user);
-		
-		return userPrincipal;
+	// private UserRepository userRepository;
+
+	public UserPrincipalDetailsService(/* UserRepository userRepository */) {
+		// this.userRepository = userRepository;
 	}
 
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+//		User user = this.userRepository.findByUsername(s);
+//		UserPrincipal userPrincipal = new UserPrincipal(user);
+//		
+//		return userPrincipal;
+
+		try {
+
+			return getUserPrincipal(username);
+
+		} catch (CustomRestExceptionMessage e) {
+			throw new CustomRestExceptionMessage(e.getLocalizedMessage());
+		} catch (Exception e) {
+			throw new CustomRestExceptionMessage("Invalid username/password combination");
+		}
+	}
+
+	public UserPrincipal getUserPrincipal(String username) {
+
+		RestTemplate restTemplate = new RestTemplate();
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8074/users-testing", String.class);
+			List<User> users = mapper.readValue(response.getBody(), mapper.getTypeFactory().constructCollectionType(List.class, User.class));
+		
+			UserPrincipal userPrincipalToReturn = new UserPrincipal();
+			
+			for (User user : users) {
+				System.out.println(user.getUsername());
+				if (user.getUsername().equals(username)) {
+					userPrincipalToReturn = new UserPrincipal(user);
+				}
+			}
+			
+			return userPrincipalToReturn;
+
+		} catch (Exception e) {
+			throw new CustomRestExceptionMessage("Failed to send REST request");
+		}
+	}
 }
