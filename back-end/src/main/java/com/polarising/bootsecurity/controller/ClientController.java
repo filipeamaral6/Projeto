fde53cba13240a7ea79321ea9c00f14a695c1c93
Filipe Amaral <filipe.amaral@polarising.com>
2019-08-05 17:35:55 +0100

    some models

--- back-end/src/main/java/com/polarising/bootsecurity/controller/ClientController.java
+++ back-end/src/main/java/com/polarising/bootsecurity/controller/ClientController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 @CrossOrigin
-public class HomeController {
+public class ClientController {
    
 }
