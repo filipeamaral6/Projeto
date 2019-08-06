d6997f914f146d38a9070cde452df9081927bee3
André Mendonça <andredmendonca89@gmail.com>
2019-08-06 10:33:55 +0100

    TIBCO - Clients and database corrected! (Please dont touch it!)

--- database/bankrising_TABLES.sql
+++ database/bankrising_TABLES.sql
@@ -61,28 +61,27 @@ CREATE TABLE `account` (
 
 CREATE TABLE `account_client` (
   `account_id` int(11) NOT NULL,
-  `client_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   KEY `account_client_account_fk` (`account_id`),
-  KEY `account_client_client_fk` (`client_id`),
+  KEY `account_client_user_fk` (`user_id`),
   CONSTRAINT `account_client_account_fk` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `account_client_client_fk` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `account_client_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `transaction` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `value` double NOT NULL,
   `account_iban` varchar(255) NOT NULL,
-  `client_cc` int(11) NOT NULL,
-  `type` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `user_id` int(11) DEFAULT NULL,
+  `type` varchar(100) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `transaction_account_fk` (`account_iban`),
-  KEY `transaction_client_cc_fk` (`client_cc`),
+  KEY `transaction_user_fk` (`user_id`),
   CONSTRAINT `transaction_account_fk` FOREIGN KEY (`account_iban`) REFERENCES `account` (`iban`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `transaction_client_cc_fk` FOREIGN KEY (`client_cc`) REFERENCES `client` (`id`)
+  CONSTRAINT `transaction_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 CREATE TABLE `deposit` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
