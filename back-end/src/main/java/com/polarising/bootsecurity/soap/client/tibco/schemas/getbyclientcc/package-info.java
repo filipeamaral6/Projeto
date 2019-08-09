4b9195af358398cc969bb3f3983dda0e192db7eb
Rui <rui.rodrigo.santos@polarising.com>
2019-08-09 12:18:40 +0100

    Front-End: Login a Bombar 3

--- back-end/src/main/java/com/polarising/bootsecurity/soap/client/tibco/schemas/getbyclientcc/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.tibco.com/schemas/tibco/Schemas/GetByClientCC.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.polarising.bootsecurity.soap.client.tibco.schemas.getbyclientcc;
