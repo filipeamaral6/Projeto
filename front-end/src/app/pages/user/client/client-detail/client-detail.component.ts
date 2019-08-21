66f07eb63a7f286b7ff44100c8de2ba1c0a41473
Filipe Amaral <filipe.amaral@polarising.com>
2019-08-21 18:50:43 +0100

    Client Details

--- front-end/src/app/pages/user/client/client-detail/client-detail.component.ts
+++ front-end/src/app/pages/user/client/client-detail/client-detail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientLayoutComponent } from 'app/layouts/client-layout/client-layout.component';
 import { Client } from 'app/shared/models/Client';
+import { ClientService } from 'app/services/transport/client.service';
+import { User } from 'app/shared/models/User';
+import { CurrentUser } from 'app/shared/models/CurrentUser';
+import { first } from 'rxjs/operators';
+import { AuthenticationService } from 'app/services/authentication.service';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -11,16 +16,58 @@ import { Client } from 'app/shared/models/Client';
 export class ClientDetailComponent implements OnInit {
 
   client: Client;
+  currentUser: CurrentUser;
+  notificationStatus = false;
 
   teste = 'ertyghjklkcmnjv hjhfbvjhbd';
-  
-  constructor(
-    private clientLayout: ClientLayoutComponent) {
-    }
 
-  ngOnInit() {
-    this.clientLayout.refreshData();
-    this.client = this.clientLayout.getClient;
+  constructor(
+    private clientLayout: ClientLayoutComponent, private clientService: ClientService, private authenticationService: AuthenticationService) {
+    this.currentUser = authenticationService.currentUserValue;
   }
 
+  ngOnInit() {
+    this.getClientInfo();
+
+  }
+
+  private getClientInfo() {
+
+    this.clientService.getById(this.currentUser.id).pipe(first()).subscribe(client => {
+      this.client = client[0];
+      this.checkNotificationStatus();
+    });
+  }
+
+  public checkNotificationStatus() {
+    if (this.client.notification === 'TRUE') {
+      this.notificationStatus = true;
+    } else {
+      this.notificationStatus = false;
+    }
+  }
+
+  public changeNotificationStatus() {
+
+    this.client.birthDate = this.client.birthDate.substring(0, 10);
+    if (this.client.notification === 'TRUE') {
+      this.client.notification = 'FALSE';
+    } else {
+      this.client.notification = 'TRUE';
+    }
+
+    console.log(this.client.notification);
+    let clientToJSON = JSON.stringify(this.client);
+
+    this.clientService.updateUser(clientToJSON)
+      .pipe(first())
+      .subscribe(
+        data => {
+          alert('Notificação por Email atualizada com sucesso!');
+        },
+        error => {
+        });
+  }
 }
+
+
