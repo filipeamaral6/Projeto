8045a31182ccb5ed138c8276f33ec9e88025a6d8
André Mendonça <andredmendonca89@gmail.com>
2019-08-22 17:02:14 +0100

    Versão MiniBug

--- back-end/src/main/java/com/polarising/bootsecurity/soap/client_account/tibco/schemas/bankrising/schemas/client/InputClient.java
+++ back-end/src/main/java/com/polarising/bootsecurity/soap/client_account/tibco/schemas/bankrising/schemas/client/InputClient.java
@@ -7,7 +7,6 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-import org.springframework.util.MultiValueMap;
 
 
 /**
