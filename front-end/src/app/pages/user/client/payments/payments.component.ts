f5a86193547a4cb6203bbdf3a6e857b4b1d42aa3
Rui <rui.rodrigo.santos@polarising.com>
2019-08-27 14:43:12 +0100

    payment a bombar!!

--- front-end/src/app/pages/user/client/payments/payments.component.ts
+++ front-end/src/app/pages/user/client/payments/payments.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from 'app/shared/models/Client';
-import { ClientLayoutComponent } from 'app/layouts/client-layout/client-layout.component';
 import { Account } from 'app/shared/models/Account';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ClientService } from 'app/services/transport/client.service';
@@ -8,7 +7,6 @@ import { AuthenticationService } from 'app/services/authentication.service';
 import { Payment } from 'app/shared/models/Payment';
 import { TransactionService } from 'app/services/transport/transaction.service';
 import { first } from 'rxjs/operators';
-import { NodeCompatibleEventEmitter } from 'rxjs/internal/observable/fromEvent';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -20,13 +18,7 @@ import { NodeCompatibleEventEmitter } from 'rxjs/internal/observable/fromEvent';
 
 export class PaymentsComponent implements OnInit {
 
-  centString: string;
-  cents: number;
-  totalValue: number;
-
-  client: Client;
-  newPayment: Payment;
-  selectedAccount: Account = null;
+  selectedAccount: Account;
   paymentForm: FormGroup;
   isEyeOpen: boolean;
   submitted = false;
@@ -34,30 +26,14 @@ export class PaymentsComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private clientService: ClientService,
     private authenticationService: AuthenticationService,
     private transactionService: TransactionService,
   ) {}
 
   ngOnInit() {
-    this.isEyeOpen = false;
-    this.newPayment = new Payment;
+    this.selectedAccount = null;
 
-    // this.client = this.clientService.getById(this.authenticationService.currentUser.id);
-
-
-    this.paymentForm = this.formBuilder.group({
-      userId: ['', Validators.required],
-      type: ['', Validators.required],
-      accountIban: ['', Validators.required],
-      clientId: ['', Validators.required],
-      value: ['', Validators.required],
-      cents: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
-      description: ['', [Validators.required, Validators.maxLength(200)]],
-      entity: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
-      reference: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
-      transactionCode: ['', Validators.required]
-    });
+    this.initForm();
   }
 
   get f() {
@@ -65,38 +41,39 @@ export class PaymentsComponent implements OnInit {
   }
 
   resetForm() {
-    console.log('Reset');
+    this.ngOnInit();
   }
 
   cancelFunction() {
     this.selectedAccount = null;
-    console.log('Cancel');
+    this.ngOnInit();
   }
 
   onSubmit() {
     console.log('Submit');
-
     this.submitted = true;
 
+    this.paymentForm.value.employeeId = 0;
+    this.paymentForm.value.type = 'Pagamento';
+    this.paymentForm.value.userId = this.authenticationService.currentUser.id;
+    this.paymentForm.value.accountIban = this.selectedAccount.iban;
+    this.paymentForm.value.accountId = this.selectedAccount.id;
 
+    this.paymentForm.value.value = this.paymentForm.value.euros + '.' + this.paymentForm.value.cents;
 
-    this.newPayment.employeeId = 0;
-    this.newPayment.type = 'Pagamento';
-    this.newPayment.userId = this.authenticationService.currentUser.id;
-    this.newPayment.accountIban = this.selectedAccount.iban;
-    this.newPayment.accountId = this.selectedAccount.id;
-    this.newPayment.value = this.newPayment.value;
-
-    this.totalValue = this.newPayment.value + (this.cents / 100);
-    this.newPayment.value = this.totalValue;
+    console.log( this.paymentForm.value );
 
     if ( this.paymentForm.invalid ) {
+      console.log(1);
+      this.ngOnInit();
       return;
     }
 
-    this.transactionService.addPayment(this.newPayment).pipe(first()).subscribe( response => {
+
+    this.transactionService.addPayment(this.paymentForm.value).pipe(first()).subscribe( response => {
       console.log(response);
     });
+    this.ngOnInit();
 
   }
 
@@ -108,8 +85,6 @@ export class PaymentsComponent implements OnInit {
     return true;
   }
 
-
-
   showPass() {
     if ( !this.isEyeOpen ) {
       this.isEyeOpen = true;
@@ -120,6 +95,24 @@ export class PaymentsComponent implements OnInit {
 
   selectAccount(account: Account) {
     this.selectedAccount = account;
+  }
+
+  initForm() {
+
+    this.isEyeOpen = false;
+
+    this.paymentForm = this.formBuilder.group({
+      employeeId: ['', Validators.required],
+      userId: ['', Validators.required],
+      type: ['', Validators.required],
+      value: ['', Validators.required],
+      euros: ['', Validators.required],
+      cents: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
+      description: ['', [Validators.required, Validators.maxLength(200)]],
+      entity: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      reference: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
+      transactionCode: ['', Validators.required]
+    });
   }
 
 }
